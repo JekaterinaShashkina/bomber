8bb62e2d3f3c0056db3739b8c07246379e75ec08
Catharin <jekaterina.shashkina@gmail.com>
2023-12-12 22:14:52 +0200

    chat, to game, board

--- server/server.js
+++ server/server.js
@@ -4,63 +4,95 @@ const socketio = require('socket.io');
 const board = require('./Map.js');
 const app = express();
 app.use(express.static(`${__dirname}/../client`));
-
 const server = http.createServer(app);
 const io = socketio(server);
 const players = {};
-const gamers = [];
+let gamers_count = 0;
+let gameTimer;
 // Парсинг JSON в теле запроса
 app.use(express.json());
 
+// Обработка запроса на лобби
 app.post('/lobby', (req, res) => {
   const userData = req.body;
   console.log('Received data:', userData);
-  // const playerId = socket.id;
 
-  res.json({ message: 'Data received successfully' });
-  gamers.push({ ...userData });
-  // console.log(gamers);
+  // Генерация уникального идентификатора для игрока
+  const playerId = Math.floor(Math.random() * 10000);
+  // Добавление игрока в список
+  players[playerId] = {
+    id: playerId,
+    userName: userData.nickname,
+    // Другие данные игрока
+  };
+  res.json({ playerId: playerId, userData: userData });
+  console.log('sended data', players[playerId]);
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected to chat');
+  const playerId = socket.handshake.query.playerId;
+  const player = players[playerId];
+  console.log('player ', player);
+  socket.join('lobby');
+  if (player) {
+    console.log(`Player ${playerId} connected`);
+    gamers_count = Object.keys(players).length;
+    console.log('Players count ', gamers_count);
 
-  socket.join('chat');
-
-  const playerId = socket.id;
-  console.log(gamers);
-  players[playerId] = {
-    id: gamers.id,
-    userName: /*`Player${Object.keys(players).length + 1}`*/ gamers.nickname,
-  };
-
-  socket.emit('message', `Welcome, ${players[playerId].username}!`);
-  console.log(`Player ${socket.id}${players[playerId].username} connected`);
-
-  socket.emit('mapUpdate', board);
-
-  // Отправляем информацию о подключившемся игроке другим игрокам
-  io.emit('playerConnected', players[playerId]);
-
-  // Обработка сообщений чата
-  socket.on('message', (data) => {
-    // Отправляем сообщение всем подключенным пользователям, включая отправителя
-    console.log(players[playerId]);
-    io.emit('message', {
-      playerName: players[playerId].userName,
-      text: data.text,
+    // Отправка информации о новом игроке в лобби
+    io.to('lobby').emit('playerConnected', player);
+    // Обработка сообщений чата
+    socket.on('message', (data) => {
+      // Отправка сообщения всем подключенным пользователям, включая отправителя
+      io.to('lobby').emit('message', {
+        playerName: player.userName,
+        text: data.text,
+      });
     });
-  });
+  } else {
+    console.log(`Unknown player tried to connect with ID: ${playerId}`);
+    // socket.emit('error', 'Invalid playerId');
+    // socket.disconnect();
+  }
+
+  // console.log('Players count ', Object.keys(players).length);
 
   // Обработка отключения игрока
   socket.on('disconnect', () => {
-    // Удаляем информацию об отключившемся игроке
+    // Удаление информации об отключившемся игроке
     delete players[playerId];
 
-    // Отправляем информацию об отключившемся игроке другим игрокам
-    io.emit('playerDisconnected', playerId);
+    // Отправка информации об отключившемся игроке другим игрокам в лобби
+    io.to('lobby').emit('playerDisconnected', playerId);
+    // Если игрок отключается и у нас есть таймер, остановите его
+    if (gameTimer) {
+      clearInterval(gameTimer);
+    }
   });
+  // Если количество игроков 2 или больше, запускаем таймер
+  if (gamers_count >= 2 && !gameTimer) {
+    startGameTimer();
+  }
 });
+// Функция для запуска таймера
+function startGameTimer() {
+  const startTime = Date.now();
+  const duration = 10000; // 10 секунд
+
+  gameTimer = setInterval(() => {
+    const elapsedTime = Date.now() - startTime;
+    const timeLeft = Math.max(0, duration - elapsedTime);
+
+    // Перенаправление на страницу игры или выполнение других действий
+    if (timeLeft === 0) {
+      console.log('Game starting now!');
+      io.to('lobby').emit('gameStart', board); // Сообщаем клиентам о начале игры
+      clearInterval(gameTimer);
+    }
+
+    io.to('lobby').emit('timerUpdate', timeLeft);
+  }, 1000);
+}
 
 server.on('error', (err) => {
   console.error(err);
